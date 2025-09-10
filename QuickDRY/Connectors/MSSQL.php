dae66c93596582ab258dd2841eaae0f3263ca61a
ben <bkucenski@gmail.com>
2025-09-10 15:10:28 -0500

    fixing handling of unnamed parameters

--- QuickDRY/Connectors/MSSQL.php
+++ QuickDRY/Connectors/MSSQL.php
@@ -34,17 +34,21 @@ class MSSQL extends strongType
         }
 
         $non_displayables = [
-//            '/%0[0-8bcef]/',            // url encoded 00-08, 11, 12, 14, 15 // this breaks LIKE '%001' for example
-//            '/%1[0-9a-f]/',             // url encoded 16-31
+            // '/%0[0-8bcef]/',            // breaks LIKE '%001'
+            // '/%1[0-9a-f]/',
             '/[\x00-\x08]/',            // 00-08
             '/\x0b/',                   // 11
             '/\x0c/',                   // 12
             '/[\x0e-\x1f]/'             // 14-31
         ];
-        foreach ($non_displayables as $regex) {
-            $data = preg_replace($regex, '', $data);
+
+        if ($data) {
+            foreach ($non_displayables as $regex) {
+                $data = preg_replace($regex, '', $data);
+            }
         }
-        $data = str_replace("'", "''", $data);
+
+        $data = str_replace("'", "''", $data ?? 'null');
         if (strcasecmp($data, 'null') == 0) {
             return 'null';
         }
@@ -63,55 +67,90 @@ class MSSQL extends strongType
      */
     public static function EscapeQuery($sql, $params, bool $test = false): string
     {
-        $pattern = '/(?<![:@])[:@]([\w\d_]+)/';
+        /**
+         * Pattern behavior:
+         *  - Eats quoted strings first (single or double) so we don't replace inside them.
+         *  - Then matches either:
+         *      a) :name or @name  (captures name in group 1)
+         *      b) a single bare @  (no capture; ensured by @(?!@))
+         *  - Guards against :: and @@ via (?<![:@]) and (?!@)
+         */
+        $pattern = '/\'(?:\'\'|[^\'])*\'|"(?:""|[^"])*"|(?<![:@])(?:[:@]([A-Za-z_][A-Za-z0-9_]*)|@(?!@))/';
+
         if ($test) {
             $matches = [];
             preg_match_all($pattern, $sql, $matches);
             Testing($matches);
         }
-        $count = 0;
-        return preg_replace_callback($pattern, function ($result)
-        use ($params, &$count, $sql) {
-            if (isset($result[1])) {
-                if (isset($params[$count])) {
-                    $count++;
-                    switch ($result[1]) {
-                        case 'nullstring':
-                            if (!$params[$count - 1] || $params[$count - 1] === 'null') {
-                                return 'null';
-                            }
-                            return MSSQL::EscapeString($params[$count - 1]);
 
-                        case 'nullnumeric':
-                            if (!$params[$count - 1] || $params[$count - 1] === 'null') {
-                                return 'null';
-                            }
-                            return $params[$count - 1] * 1.0;
+        $posValues = array_values($params ?? []);
+        $posIndex  = 0;
 
-                        case 'nq':
-                            return $params[$count - 1];
+        return preg_replace_callback($pattern, function ($m) use (&$posIndex, $posValues, $params) {
+            $tok = $m[0];
 
-                        default:
-                            return MSSQL::EscapeString($params[$count - 1]);
+            // 1) Quoted string? Return as-is.
+            $c0 = $tok[0];
+            if ($c0 === "'" || $c0 === '"') {
+                return $tok;
+            }
+
+            // 2) Named placeholder (@name or :name)
+            if (isset($m[1]) && $m[1] !== '') {
+                $name = $m[1];
+
+                // Prefer named binding if provided
+                if (array_key_exists($name, $params)) {
+                    if (Strings::EndsWith($name, '_NQ')) {
+                        return $params[$name];
                     }
-                }
-
-                if (isset($params[$result[1]])) {
-                    if (Strings::EndsWith($result[1], '_NQ')) {
-                        return $params[$result[1]];
-                    }
-                    return MSSQL::EscapeString($params[$result[1]]);
-                } else {
-                    if($params && is_null($params[$result[1]] ?? null)) {
+                    $val = $params[$name];
+                    if ($val === null) {
                         return 'null';
                     }
-                    // in order to allow more advanced queries that Declare Variables, we just need to ignore @Var if it's not in the passed in parameters
-                    // SQL Server will return an error if there really is one
-                    return '@' . $result[1];
+                    return MSSQL::EscapeString($val);
                 }
-                //throw new Exception(print_r([json_encode($params, JSON_PRETTY_PRINT), $count, $result, $sql], true) . ' does not have a matching parameter (ms_escape_query).');
+
+                // Fallback to positional consumption if available
+                if (array_key_exists($posIndex, $posValues)) {
+                    $val = $posValues[$posIndex++];
+                    switch ($name) {
+                        case 'nullstring':
+                            if ($val === null || $val === 'null' || $val === '') {
+                                return 'null';
+                            }
+                            return MSSQL::EscapeString($val);
+
+                        case 'nullnumeric':
+                            if ($val === null || $val === 'null' || $val === '') {
+                                return 'null';
+                            }
+                            return $val * 1.0;
+
+                        case 'nq':
+                            return $val;
+
+                        default:
+                            return MSSQL::EscapeString($val);
+                    }
+                }
+
+                // Allow declared SQL variables to pass through if no binding
+                return '@' . $name;
             }
-            return '';
+
+            // 3) Bare @ → consume next positional value
+            if (array_key_exists($posIndex, $posValues)) {
+                $val = $posValues[$posIndex++];
+                // Bare @ doesn't have special name-based modes; just escape/null as needed
+                if ($val === null) {
+                    return 'null';
+                }
+                return MSSQL::EscapeString($val);
+            }
+
+            // No positional value available; leave a single @ (likely a SQL var)
+            return '@';
         }, $sql);
     }
 }
